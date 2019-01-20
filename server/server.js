5899b970c75571cc0dccfc0ffd9e10babe1a45ec
BuildTools <unconfigured@null.spigotmc.org>
2019-01-20 00:35:27 -0800

    Updated add and delete, removed create.

---
+++ server/server.js
@@ -0,0 +1,37 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var todos = [];
+
+app.get('/', function(req, res) {
+   res.sendfile('index.html');
+});
+
+io.on('connection', function(socket) {
+    socket.on('add', function(data) {
+        //Merge create
+        todos.push(data);
+        console.log(todos);
+    });
+
+    socket.on('delete', function(data) {
+        todos.pop();
+        console.log(todos);
+    });
+
+    socket.on('update', function(data) {
+        todos.pop();
+        todos.push(data);
+        console.log("TODO: Update");
+    });
+});
+
+http.listen(8000, function() {
+   console.log('listening on localhost:8000');
+});
+
+//Is the task sent?
+//Is it clicked?
+//How much time?
+//ID
